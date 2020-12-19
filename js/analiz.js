ba0fb25aa1b09c98f64b9b15b5908a72ed5dbd7c
Zelenskiyas <zelensk1973@gmail.com>
2020-12-19 22:43:13 +0200

    feat: add work on functional for analis lessons 2

--- js/analiz.js
+++ js/analiz.js
@@ -1,6 +1,6 @@
 var d1 = "";
 let data_table = {};
-var data_report = [];
+
 
 let sheet2 = "default";
 let title_st = '';
@@ -13,50 +13,76 @@ function sortById(arr) {
 }
 
 function print_my(data){
-    text.innerHTML ="";
+    data.push({'id':111111111, 'title':" ", 'value':" "});
+    text.innerHTML +=`<br><hr><br><div class="title1">АНАЛІЗ УРОКУ</div>`;
     for(let i=0; i<data.length; i++){
         if (data[i]['value'] !== "" && data[i]['id'] >= 0){
-            console.log(data[i]['title']);  
-            console.log(data[i]['value']);
-             text.innerHTML += `<div class="title">${data[i]['title']}</div> 
-             <div class="value">${data[i]['value']}</div>`;
+            // text.innerHTML += `<span class="title">${data[i]['title']}</span> 
+            //                    <span class="value">${data[i]['value']}</span><br>`;
+            let tag1 = 'span';
+            let tag2 = 'span';
+            let sep = ' '
+            // console.log(data[i+1]);
+            if (data[i+1] !== undefined){
+                 let sym1 =(data[i+1]['title'] +" ")[0];
+                 let sym2 =(data[i]['title'] +" ")[0];
+                 if (sym2.toUpperCase() === sym2){
+                     tag2 = 'span';
+                    sep = "<br>";
+                 }
+            }
+            
 
-        }
-                 
+            text.innerHTML += `${sep}<${tag1} class="title">${data[i]['title']}</${tag1}> 
+                                <${tag2} class="value">${data[i]['value']}</${tag2}>`;
+        }                 
     }
+    text.innerHTML +=`<br> 
+                      <div class="footer">З аналізом ознайомлений(на) _______________________ </div>`;
 }
 
 let num = 4
 
+function createCard(dat){
+    let data_report = []
+    $.each(dat,function(key, value){
+        if (value['title'] !== "" && data_table[key] !== undefined){
+            let id = data_table[key]['id'];
+            let title = data_table[key]['title'];
+            data_report.push({'id':id, 'title':title, 'value':value['$t']})
+        }            
+                    
+    });
+    sortById(data_report);
+    print_my(data_report);
+}
 
 function readPage(){
     let url  = "https://spreadsheets.google.com/feeds/list/"+key+"/"+sheet2+"/public/values?alt=json"
     $.getJSON(url,
         
        function (data) {
+            console.log(data);
             data = data['feed']['entry'];
-
+            
             $.each(data[0],function(key,value){
                 if (key.indexOf('gsx$') === 0){
-                    // console.log(data[1]);
                     let m = {'id': parseFloat(data[1][key]['$t']), 'title':value['$t']};
                     data_table[key] = m;                   
                 }              
             });
             // console.log(data_table);
-            data_report = [];
-            
-            $.each(data[num],function(key,value){
-                if (value['title'] !== "" && data_table[key] !== undefined){
-                    let id = data_table[key]['id'];
-                    let title = data_table[key]['title'];
-                    data_report.push({'id':id, 'title':title, 'value':value['$t']})
-                }              
-                        
-            });
-            sortById(data_report);
+            text.innerHTML = "";
 
-            print_my(data_report);
+            for (let i=3; i<data.length;i++){
+                createCard(data[i]);
+            }
+            
+
+            
+
+
+            
 
         }       
     );    
