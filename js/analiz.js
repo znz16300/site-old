556f192bbd7f5979e4189d2c857f9d40f26ece59
Zelenskiyas <zelensk1973@gmail.com>
2020-12-20 12:23:49 +0200

    feat: add work on functional for analis lessons 3

--- js/analiz.js
+++ js/analiz.js
@@ -45,6 +45,9 @@ let num = 4
 
 function createCard(dat){
     let data_report = []
+    if (num === -1) {
+        
+    }
     $.each(dat,function(key, value){
         if (value['title'] !== "" && data_table[key] !== undefined){
             let id = data_table[key]['id'];
@@ -57,6 +60,17 @@ function createCard(dat){
     print_my(data_report);
 }
 
+function createCards(data, num=-1){
+    text.innerHTML = "";
+    if (num === -1) {
+        for (let i=3; i<data.length;i++){
+            createCard(data[i]);
+        }
+    } else {
+        createCard(data[num]);
+    }
+}
+var gl_data;
 function readPage(){
     let url  = "https://spreadsheets.google.com/feeds/list/"+key+"/"+sheet2+"/public/values?alt=json"
     $.getJSON(url,
@@ -64,33 +78,81 @@ function readPage(){
        function (data) {
             console.log(data);
             data = data['feed']['entry'];
-            
+            gl_data = data;
             $.each(data[0],function(key,value){
                 if (key.indexOf('gsx$') === 0){
                     let m = {'id': parseFloat(data[1][key]['$t']), 'title':value['$t']};
                     data_table[key] = m;                   
                 }              
             });
-            // console.log(data_table);
-            text.innerHTML = "";
 
-            for (let i=3; i<data.length;i++){
-                createCard(data[i]);
-            }
-            
+            createTable(data);
 
-            
-
-
-            
+            // createCards(data);       
 
         }       
     );    
-    // print(data_report);
+
 }
-// console.log(data_report);
+
+const btn_print = document.getElementById("btn__print_id");
+const btn_teach = document.getElementById("id_table_teach");
+btn_print.addEventListener("click", ()=>{
+    
+});
+
+btn_print.addEventListener("click", ()=>{
+    text.innerText = "";
+    for (let i=3; i<gl_data.length; i++){
+        let c = document.getElementById("id_"+String(i));
+        if (c.checked){
+            createCard(gl_data[i]);
+        }
+    }
+
+    
+})
+
+function createTable(data){
+    console.log(data);
+    const table = document.getElementById("table__id");
+    for (let i=3; i<data.length; i++){
+        let row = document.createElement('tr');
+        let cel1 = document.createElement('td');
+        let cel2 = document.createElement('td');
+        let cel3 = document.createElement('td');
+        let cel4 = document.createElement('td');
+        let cel5 = document.createElement('td');
+        let cel6 = document.createElement('td');
+        let cel7 = document.createElement('td');
+        cel1.classList.add('cel_def');
+        cel2.classList.add('cel_def');
+        cel3.classList.add('cel_def');
+        cel4.classList.add('cel_def');
+        cel5.classList.add('cel_def');
+        cel6.classList.add('cel_def');
+        cel7.classList.add('cel_def');
+        
+        cel1.innerText=data[i]['gsx$датапроведенняуроку']['$t'];
+        cel2.innerText=data[i]['gsx$хтовідвідуєурок']['$t'];
+        cel3.innerText=data[i]['gsx$вчительурокякоговідвідують']['$t'];
+        cel4.innerText=data[i]['gsx$класгрупа']['$t'];
+        cel6.innerText=data[i]['gsx$темауроку']['$t'];
+        cel5.innerText=data[i]['gsx$предмет']['$t'];
+        cel7.innerHTML=`
+            <input id="id_${String(i)}" type="checkbox">
+        `;
+
+        row.append(cel1, cel2, cel3, cel4, cel5, cel6, cel7)
+        table.append(row);
+
+    }
+    
+ 
 
 
+}
 
 readPage();
+// createCards(data);
 
