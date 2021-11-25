4d8c586c2e7740dbae57604b8550fc42a505101a
znz16300 <75092639+znz16300@users.noreply.github.com>
2021-11-25 14:56:00 +0200

    Update analiz_t.js

--- js/analiz_t.js
+++ js/analiz_t.js
@@ -1,5 +1,5 @@
-const url = "http://127.0.0.1:5000/";
-//const url = "https://schooltools.pythonanywhere.com/";
+//const url = "http://127.0.0.1:5000/";
+const url = "https://schooltools.pythonanywhere.com/";
 
 var d1 = "";
 // let data_table = {};
