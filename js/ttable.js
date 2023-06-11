08fa80cb77be03367f036ce4a4171ff0ebc4a8c8
Zelenskiyas <zelensk1973@gmail.com>
2023-06-11 21:17:32 +0300

    fix: 1

--- js/ttable.js
+++ js/ttable.js
@@ -70,30 +70,37 @@ btnLeft.addEventListener('click', () => {
   shiftDate(datePicker, 1);
   const tWD = getData(glData, 'workdays')['data'];
   day  = getDataByDate(tWD, datePicker.value);
-  showTT();
+  if (teacherList.value !== '')
+    showTT(teacherList, ['week1', 'week2']);
+  else
+    showTT(clasList, ['week1 (clas)', 'week2 (clas)']);
+
 });
 
 btnRight.addEventListener('click', () => {
   shiftDate(datePicker, -1);
   const tWD = getData(glData, 'workdays')['data'];
   day  = getDataByDate(tWD, datePicker.value);
-  showTT();
+  if (teacherList.value !== '')
+    showTT(teacherList, ['week1', 'week2']);
+  else
+    showTT(clasList, ['week1 (clas)', 'week2 (clas)']);
 });
 
 datePicker.addEventListener('change', () => {
   const tWD = getData(glData, 'workdays')['data'];
   day  = getDataByDate(tWD, datePicker.value);
-  showTT();
+  showTT(teacherList, ['week1', 'week2']);
 })
 
 function showTT(listD, tables){
   lessList.innerHTML = '<li value=""></li>';
-  const selectedOption = listD.options[clasList.selectedIndex];
+  const selectedOption = listD.options[listD.selectedIndex];
   const surname = selectedOption.text;
   // Шукаємо розклад вчителя
   console.log(surname);
   let week = tables[0];
-  if (day.chZn == 2){
+  if (day.chZn == "2"){
     week = tables[1];
   }
   const list = getData(glData, week)['data'];
@@ -117,67 +124,28 @@ function showTT(listD, tables){
   console.log(startLesson)
   for (let i=start; i<fin; i++){    
     let lesLime = startLesson[0][(i-1)%(maxLessons+1)]
+    if (lesLime === undefined) lesLime = '';
     let num =  tt[2][i]
+    if (num === undefined) num = '';
     let klas =  tt[1][i]
-    let subj =  tt[0][i]
-    if (lesLime === undefined) lesLime = '-';
-    if (num === undefined) num = '-';
-    if (klas === undefined) klas = '-';
-    if (subj === undefined) subj = '-';
-
-    lList.push("(" + lesLime + ") " + num + ". " + klas + " - " + subj)
+    if (klas === undefined) klas = '';
+    let subj =  tt[0][i]   
+    if (subj === undefined) subj = '';
+    // if (subj === '') klas = '-'
+    if (tables[0].length > 5) //Ознака того, що розклад класу
+      lList.push("(" + lesLime + ") " + num + ". " + subj );
+    else
+      lList.push("(" + lesLime + ") " + num + ". " + klas + " - " + subj)
   }
   createList(lessList, lList, 'li')
 }
 
-// function showTeachersTT(){
-//   lessList.innerHTML = '<li value=""></li>';
-//   const selectedOption = teacherList.options[teacherList.selectedIndex];
-//   const surname = selectedOption.text;
-//   // Шукаємо розклад вчителя
-//   console.log(surname);
-//   let week = 'week1';
-//   if (day.chZn == 2){
-//     week = 'week2';
-//   }
-//   const list = getData(glData, week)['data'];
-//   const tt = getInnerListByName(list, surname)
-  
-//   // Визначаємо діапазон уроків
-//   const  maxLessons = 11;
-//   let lList = [];
-  
-//   let start = (day.dWeek-1)*(maxLessons+1)+1;  
-//   let fin = start + maxLessons;
-//   let s;
-//   console.log(day);
-//   if (day.chZn == 1) 
-//     s =  tt[3][start-1] + ". Чисельник";
-//   else 
-//     s =  tt[3][start-1] + ". Знаменник";
-//   title.innerText = s;
- 
-//   const startLesson = getData(glData, 'Час початку уроків').header;
-//   console.log(startLesson)
-//   for (let i=start; i<fin; i++){    
-//     let lesLime = startLesson[0][(i-1)%(maxLessons+1)]
-//     let num =  tt[2][i]
-//     let klas =  tt[1][i]
-//     let subj =  tt[0][i]
-//     if (lesLime === undefined) lesLime = '-';
-//     if (num === undefined) num = '-';
-//     if (klas === undefined) klas = '-';
-//     if (subj === undefined) subj = '-';
-
-//     lList.push("(" + lesLime + ") " + num + ". " + klas + " - " + subj)
-//   }
-//   createList(lessList, lList, 'li')
-// }
-
 teacherList.addEventListener('change', ()=>{
+  clasList.value = ''
   showTT(teacherList, ['week1', 'week2']);
 })
 clasList.addEventListener('change', ()=>{
+  teacherList.value = ''
   showTT(clasList, ['week1 (clas)', 'week2 (clas)']);
 })
 
@@ -257,3 +225,7 @@ function createList(node, list, tag) {
 
 
 readTT();
+
+
+// http://127.0.0.1:5509/ttable.html?id=1obSD_Q_w6ZXVAfMmJyXsGkf12VqDWjdhLDwARsd9Ujk
+
